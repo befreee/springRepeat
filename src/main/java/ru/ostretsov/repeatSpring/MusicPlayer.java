33921342b4757f0610bf041af940b618d334abbf
befreee <ostretsovalexander@gmail.com>
2021-06-15 15:17:00 +0300

    les6_DI_list_by_xml

--- src/main/java/ru/ostretsov/repeatSpring/MusicPlayer.java
+++ src/main/java/ru/ostretsov/repeatSpring/MusicPlayer.java
@@ -1,9 +1,20 @@
 package ru.ostretsov.repeatSpring;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
     private String name;
     private int volume;
+
+    public List<Music> getMusicList() {
+        return musicList;
+    }
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
 
     public String getName() {
         return name;
@@ -21,20 +32,14 @@ public class MusicPlayer {
         this.volume = volume;
     }
 
-    // IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
     public MusicPlayer(){
 
     }
 
-    public void setMusic(Music music) {
-        this.music = music;
-    }
 
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        for (Music music:musicList){
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
