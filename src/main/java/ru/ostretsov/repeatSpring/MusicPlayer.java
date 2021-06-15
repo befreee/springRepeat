13fb48efca5f1a1058da75bb33259c7609c3736a
befreee <ostretsovalexander@gmail.com>
2021-06-15 14:13:58 +0300

    les5_end

--- src/main/java/ru/ostretsov/repeatSpring/MusicPlayer.java
+++ src/main/java/ru/ostretsov/repeatSpring/MusicPlayer.java
@@ -2,13 +2,39 @@ package ru.ostretsov.repeatSpring;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
 
     // IoC
-    public MusicPlayer(Music music){
+    public MusicPlayer(Music music) {
         this.music = music;
     }
 
-    public void playMusic(){
+    public MusicPlayer(){
+
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public void playMusic() {
         System.out.println("Playing: " + music.getSong());
     }
 }
